8648d659ce0a392338be7372cd40d277504540bd
Akeem Flavors <akeemflavors@gmail.com>
2020-10-05 21:01:28 -0400

    Sign up error/success messages

--- client/src/components/pages/SignUp.js
+++ client/src/components/pages/SignUp.js
@@ -7,6 +7,8 @@ const SignUpForm = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [name, setName] = useState("");
+    const [signupError, setError] = useState(false)
+    const [signupSuccess, setSuccess] = useState(false)
 
     const onSubmit = event => {
         event.preventDefault();
@@ -21,10 +23,13 @@ const SignUpForm = () => {
             .post("/api/users/signup", userData)
             .then(res => {
                 console.log(res);
+                setSuccess(true)
+                setError(false)
             })
             .catch(err => {
                 console.log(err);
                 console.log(err.response);
+                setError(true)
             });
     };
 
@@ -32,53 +37,63 @@ const SignUpForm = () => {
         <Fragment>
             <SignupHeader />
             <Container>
-    <Grid textAlign='center' style={{ height: '10vh' }} verticalAlign='middle'>
-        <Grid.Column style={{ maxWidth: 450 }}>
-        <Header as='h2' color='grey' textAlign='center'>
-            <Icon name='edit' /> Sign Up For An Account
-        </Header>
-        <Form size='large' onSubmit={onSubmit}>
-            <Segment stacked>
-            <Form.Input 
-            fluid icon='user' 
-            iconPosition='left' 
-            placeholder='Name'
-            onChange={event => {
-                setName(event.target.value);
-                console.log(name);
-            }}
-            //Do I need required here?
-            required
-            />
-            <Form.Input 
-            fluid icon='mail' 
-            iconPosition='left' 
-            placeholder='E-mail address'
-            onChange={event => {
-                setEmail(event.target.value);
-                console.log(email);
-            }}
-            //Do I need required here?
-            required
-            />
-            <Form.Input
-                fluid icon='lock'
-                iconPosition='left'
-                placeholder='Password'
-                type='password'
-                onChange={event => setPassword(event.target.value)}
-            />
+                <Grid textAlign='center' style={{ height: '10vh' }} verticalAlign='middle'>
+                    <Grid.Column style={{ maxWidth: 450 }}>
+                    <Header as='h2' color='grey' textAlign='center'>
+                        <Icon name='edit' /> Sign Up For An Account
+                    </Header>
+                    <Form size='large' onSubmit={onSubmit} error={signupError} success={signupSuccess}>
+                        <Segment stacked>
+                        <Form.Input 
+                        fluid icon='user' 
+                        iconPosition='left' 
+                        placeholder='Name'
+                        onChange={event => {
+                            setName(event.target.value);
+                            console.log(name);
+                        }}
+                        //Do I need required here?
+                        required
+                        />
+                        <Form.Input 
+                        fluid icon='mail' 
+                        iconPosition='left' 
+                        placeholder='E-mail address'
+                        onChange={event => {
+                            setEmail(event.target.value);
+                            console.log(email);
+                        }}
+                        //Do I need required here?
+                        required
+                        />
+                        <Form.Input
+                            fluid icon='lock'
+                            iconPosition='left'
+                            placeholder='Password'
+                            type='password'
+                            onChange={event => setPassword(event.target.value)}
+                        />
+                        <Message
+                            error
+                            header='Error'
+                            content='Please check credentials try again.'
+                        />
+                        <Message 
+                            success
+                            header="Account Registered"
+                            content="You are signed up and can now log in" 
+                        />    
 
-            <Button color='grey' fluid size='large'>
-                Sign Up
-            </Button>
-            </Segment>
-        </Form>
-        <Message>
-            Already have an account? <a href='/login'>Log-in</a>
-        </Message>
-        </Grid.Column>
-    </Grid>
+                        <Button color='grey' fluid size='large'>
+                            Sign Up
+                        </Button>
+                        </Segment>
+                    </Form>
+                    <Message>
+                        Already have an account? <a href='/login'>Log In</a>
+                    </Message>
+                    </Grid.Column>
+                </Grid>
             </Container>
         </Fragment>
     )
